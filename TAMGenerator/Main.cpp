4667b43866e0b14b065964ab73f3c2f0c5e6b970
InflamedBanana <no>
2017-11-09 19:50:06 +0100

    test

--- TAMGenerator/Main.cpp
+++ TAMGenerator/Main.cpp
@@ -1,4 +1,6 @@
 #include <CImg/CImg.h>
+#include <vector>
+#include <iostream>
 
 using namespace cimg_library;
 
@@ -8,6 +10,58 @@ struct tamOptions
 	unsigned int maxMapSize;
 	unsigned int minMapSize;
 	unsigned char greyLvl;
+};
+
+struct Position
+{
+	float x;
+	float y;
+};
+
+class Object
+{
+public :
+	Object() : m_position(), m_opacity(1), m_hardness(1){}
+	Object(Position _position) :m_position(_position), m_opacity(1), m_hardness(1) {}
+	Object(float _opacity, float _hardness) :m_opacity(_opacity), m_hardness(_hardness){}
+	Object(Position _position, float _opacity, float _hardness) : m_position(_position), m_opacity(_opacity), m_hardness(_hardness) {}
+
+protected :
+	float m_opacity;
+	float m_hardness;
+	Position m_position;
+
+
+public:
+	float opacity() const { return m_opacity; }
+	float opacity(float value) { if (value > 1)m_opacity = 1; else if (value < 0) m_opacity = 0; else m_opacity = value; }
+	float hardness() const { return m_hardness; }
+	float hardness(float value){ if (value > 1) m_hardness= 1; else if (value < 0) m_hardness= 0; else m_hardness = value; }
+	Position position() { return m_position; }
+};
+
+class Point : public Object
+{
+private:
+	unsigned int m_radius;
+
+public:
+	int radius() const { return m_radius; }
+	int radius(int value) { m_radius = value; }
+
+	Point(unsigned int _radius): Object(), m_radius(_radius) {}
+	Point(unsigned int _radius, float _opacity, float _hardness ): Object( _opacity,_hardness ), m_radius(_radius) {}
+	Point(Position _position, unsigned int _radius, float _opacity, float _hardness) : Object(_position, _opacity, _hardness), m_radius(_radius) {}
+};
+
+class Line : public Object
+{
+private:
+	unsigned int m_length;
+
+public:
+	unsigned int length() const { return m_length; }
+	unsigned int length(unsigned int value) { m_length = value; }
 };
 
 int main() {
@@ -28,12 +82,30 @@ int main() {
 		}
 	}*/
 
-	CImg<unsigned char> rsltImage(32, 32, 1, 1, 255);
-	const unsigned char red[] = { 255,0,0 }, green[] = { 0,255,0 }, blue[] = { 0,0,255 };
+	int radius;
+	float opacity;
+	float hardness;
 
-	CImgDisplay rslt_display(512, 512, "Rslt"), opt_display(500, 250, "Options");
+	std::cout << "Radius :" << std::endl;
+	std::cin >> radius;
+	std::cout << "Opacity : [0-1]" << std::endl;
+	std::cin >> opacity;
+	std::cout << "Hardness : [0-1]" << std::endl;
+	std::cin >> hardness;
 
-	rsltImage.draw_line(5, 15, 32, 15, blue).display(rslt_display);
+
+	CImg<unsigned char> rsltImage(32, 32, 1, 1, 0);
+	const unsigned char black[] = { 0,0,0 }, white[] = { 255,255,255 }, blue[] = { 0,0,255 };
+	Position pos;
+	pos.x = pos.y = .5f;
+	
+	Point p( pos, radius, opacity, 1.f-hardness);
+	
+
+	CImgDisplay rslt_display(512, 512, "Rslt"), opt_display( 500, 250, "Options" );
+
+	rsltImage.draw_circle(rsltImage.width() * p.position().x, rsltImage.height() * p.position().y, p.radius(), white, p.opacity() ).blur(p.hardness()).display(rslt_display);
+	//rsltImage.draw_line(5, 15, 32, 15, blue).display(rslt_display);
 	tamOptions opt;
 
 	while (!rslt_display.is_closed() && !opt_display.is_closed() ) 
